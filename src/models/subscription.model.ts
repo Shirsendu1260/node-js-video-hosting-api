69423064ddb31a5d304a4528432383d5bb52bee1
Shirsendu Mali <shirsendu1260@gmail.com>
2026-03-23 19:57:01 +0530

    Added aggregation notes

--- src/models/subscription.model.ts
+++ src/models/subscription.model.ts
@@ -41,8 +41,8 @@ export type { SubscriptionDocument, SubscriptionModel };
 }
 */
 
-// When an user subscribes another channel (user), a single docuent of this type will be created
-// When a channel is subscribed by another channel (user), a single docuent of this type will be created
+// When an user subscribes another channel (user), a single docuMent of this type will be created
+// When a channel is subscribed by another channel (user), a single docuMent of this type will be created
 
 /* User 'a' subscribes channel 'CNLA':
 {
@@ -96,3 +96,65 @@ export type { SubscriptionDocument, SubscriptionModel };
     { subscriber: 'c', channel: 'CNLC' }   */
 // Count = 3
 // FORMULA: In this model, find the documents to count subscribed CHANNELs of a user that has matching SUBSCRIBER
+
+
+
+/*******************  MONGODB AGGREGATION EXAMPLE  *******************/
+// db.books.aggregate([
+//     /**
+//      * STAGE 1: $lookup (The "Join" Stage)
+//      * Combines data from another collection.
+//      * Note: MongoDB ALWAYS returns the result as an ARRAY [ ],
+//      * even if only one match is found.
+//      */
+//     {
+//         $lookup: {
+//             from: 'authors',          // The collection to pull data FROM
+//             localField: 'author_id',  // The FK (foreign key) in 'books'
+//             foreignField: '_id',      // The PK (primary key) in 'authors'
+//             as: 'author_details'      // The name for the new array field
+//         }
+//     },
+
+//     /**
+//      * STAGE 2: $addFields + $first (The "Cleaning" Stage)
+//      * Since $lookup gives us an array like [ {name: "Krishna"} ],
+//      * we use $first to extract the object inside.
+//      * This makes it a single object {name: "Orwell"} for easier access.
+//      */
+//     {
+//         $addFields: {
+//             author_details: { 
+//                 $first: '$author_details' 
+// 				   // $arrayElemAt: ['$author_details', 0] // same as last step
+//             }
+//         }
+//     }
+// ]);
+
+/**
+ * VISUAL EXAMPLE
+ * * 1. YOUR COLLECTIONS:
+ * Books:   { "title": "1984", "author_id": 50 }
+ * Authors: { "_id": 50, "name": "Krishna" }
+ * * 2. AFTER STAGE 1 ($lookup):
+ * {
+ * "title": "1984",
+ * "author_id": 50,
+ * "author_details": [ { "_id": 50, "name": "Krishna" } ]
+ * }
+ * // ^ Note the [ brackets ]. It is a list.
+ * * 3. AFTER STAGE 2 ($first):
+ * {
+ * "title": "1984",
+ * "author_id": 50,
+ * "author_details": { "_id": 50, "name": "George Orwell" }
+ * }
+ * // ^ Brackets gone. It is now a clean object.
+ */
+
+/**
+ * QUICK COMPARISON:
+ * - $first: Keeps the book even if the author doesn't exist (Left Join).
+ * - $unwind: Deletes the book from results if the author doesn't exist (Inner Join).
+ */
