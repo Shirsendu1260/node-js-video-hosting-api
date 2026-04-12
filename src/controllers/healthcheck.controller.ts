1e73fc7e373636c672973d050288bfce1b6a5a51
Shirsendu Mali <shirsendu1260@gmail.com>
2026-04-12 13:12:44 +0530

    Added healthcheck endpoint

--- src/controllers/healthcheck.controller.ts
+++ src/controllers/healthcheck.controller.ts
@@ -1,11 +1,12 @@
 import { asyncHandler } from '../utils/asyncHandler.js';
-import { ApiError } from '../utils/ApiError.js';
 import { ApiResponse } from '../utils/ApiResponse.js';
 
 
-
-const healthcheck = asyncHandler(async (req, res) => {
-    
+const healthcheck = asyncHandler(async (_, res) => {
+	return res.status(200).json(
+        new ApiResponse(200, {}, 'STATUS: ALL OK')
+    );
 });
+
 
 export { healthcheck };
