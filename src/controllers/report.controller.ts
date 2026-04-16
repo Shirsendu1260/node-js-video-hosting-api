b5c17501a9c7bf5315b13ac9847d58a3783ae646
Shirsendu Mali <shirsendu1260@gmail.com>
2026-04-16 23:08:20 +0530

    Added report endpoints and its DB model

---
+++ src/controllers/report.controller.ts
@@ -0,0 +1,290 @@
+import { asyncHandler } from '../utils/asyncHandler.js';
+import { ApiError } from '../utils/ApiError.js';
+import { ApiResponse } from '../utils/ApiResponse.js';
+import { Report } from '../models/report.model.js';
+import type { IErrorMessage } from "../utils/ApiError.js";
+import getBase64DecodedId from "../utils/decodeBase64Id.js";
+import Joi from 'joi';
+import mongoose from 'mongoose';
+
+
+
+// For submitting a new report for a Video or Post
+const submitReport = asyncHandler(async (req, res) => {
+	const { targetId, targetModel, reason, details } = req.body as {
+		targetId: string,
+		targetModel: string,
+		reason: string,
+		details?: string
+	};
+
+	if(!req.user) {
+        throw new ApiError(401, 'You need to be authenticated to report this content.');
+    }
+
+    if(!mongoose.Types.ObjectId.isValid(targetId)) {
+    	throw new ApiError(400, 'Invalid target ID.');
+    }
+
+	const validatorSchema = Joi.object({
+		targetModel: Joi.string()
+                        .trim()
+                        .valid('Video', 'Post')
+                        .required()
+                        .messages({
+                          'any.only': 'targetModel must be Video or Post.',
+                          'any.required': 'targetModel is required.'
+                        }),
+    	reason: Joi.string()
+                    .trim()
+                    .valid('Hate Speech', 'Harassment', 'Violence', 'Spam', 'Scam', 'Inappropriate', 'Other')
+                    .required()
+                    .messages({
+                      'any.only': 'Reason must be Hate Speech, Harassment, Violence, Spam, Scam, Inappropriate or Other.',
+                      'any.required': 'Reason is required.'
+                    }),
+        details: Joi.string()
+                    .trim()
+                    .max(5000)
+                    .allow('')
+                    .messages({
+                        'string.max': 'Details cannot exceed 5000 characters.',
+                    })
+	});
+
+	const { error } = validatorSchema.validate(
+        { targetModel, reason, details },
+        { abortEarly: false }
+    );
+
+    if(error) {
+        const errorArray: IErrorMessage[] = error.details.map(detail => {
+            return { 
+                [detail.path[0] as string]: detail.message 
+            }
+        });
+
+        console.log(errorArray);
+        throw new ApiError(400, 'Report submit validation failed.', errorArray);
+    }
+
+    // Check if user has already made report for this content
+    const existingReport = await Report.findOne({
+    	reporter: new mongoose.Types.ObjectId(req.user._id),
+    	targetId: new mongoose.Types.ObjectId(targetId)
+    });
+
+    if(existingReport) {
+        throw new ApiError(400, 'You have already reported this content earlier. Our team is reviewing it.');
+    }
+
+    // Create report
+    const reportCreated = await Report.create({
+        reporter: req.user._id,
+        targetId,
+        targetModel,
+        reason,
+        details,
+        status: 'PEND'
+    });
+
+    return res.status(201).json(
+        new ApiResponse(201, reportCreated, 'Report sent successfully.')
+    );
+});
+
+
+
+const updateReportStatus = asyncHandler(async (req, res) => {
+    const { reportId } = req.params as { reportId: string };
+    const { status } = req.body as { status: string };
+
+    const decodedReportId = getBase64DecodedId(reportId);
+
+    if(req.user?.username !== process.env.ADMIN_USERNAME) {
+        throw new ApiError(403, 'You are not authorized to update report status.');
+    }
+
+    const statusValidation = ['PEND', 'REV', 'RES'].includes(status);
+
+    if(!statusValidation) {
+        throw new ApiError(400, 'Invalid status update.');
+    }
+
+    const reportUpdated = await Report.findByIdAndUpdate(
+        decodedReportId,
+        {
+            $set: { status }
+        },
+        {
+            returnDocument: 'after',
+            runValidators: true
+        }
+    );
+
+    if(!reportUpdated) {
+        throw new ApiError(404, 'Report not found.');
+    }
+
+    return res.status(200).json(
+        new ApiResponse(200, reportUpdated, `Report status (status: '${status}') update successfully.`)
+    );
+});
+
+
+
+const getAllReports = asyncHandler(async (req, res) => {
+    let { 
+        page = 1, 
+        limit = 4,
+        status,
+        targetModel
+    } = req.query as {
+        page?: string,
+        limit?: string,
+        status?: string,
+        targetModel?: string
+    };
+
+    const pageNo = Number(page);
+    const limitCount = Number(limit);
+
+    if(req.user?.username !== process.env.ADMIN_USERNAME) {
+        throw new ApiError(403, 'You are not authorized to view user reports.');
+    }
+
+    if(status) {
+        const statusValidation = ['PEND', 'REV', 'RES'].includes(status);
+        if(!statusValidation) {
+            throw new ApiError(400, 'Invalid status update.');
+        }
+    }
+
+    let matchCondition: { status?: string, targetModel?: string } = {};
+    if(status) matchCondition.status = status;
+    if(targetModel) matchCondition.targetModel = targetModel;
+
+    const reportAggregate = Report.aggregate([
+        // Match by report status or target model (Video/Post)
+        {
+            $match: matchCondition
+        },
+
+        // Lookup to get reporter (user) details for each report
+        {
+            $lookup: {
+                from: 'users',
+                localField: 'reporter',
+                foreignField: '_id',
+                as: 'reporter',
+                pipeline: [
+                    {
+                        $project: {
+                            username: 1,
+                            fullName: 1,
+                            avatar: 1
+                        }
+                    }
+                ]
+            }
+        },
+
+        // Flatten the user lookup array
+        {
+            $addFields: {
+                reporter: { $first: '$reporter' }
+            }
+        },
+
+        // Lookup to get content (Video/Post) details
+        // Since we are using 'refPath', the 'from' field need to be dynamic
+        // Aggregation does not support dynamic 'from' in a single $lookup easily
+        // so we use $lookup for both, then we choose which one to keep based on admin's request (targetModel)
+        
+        // 1. Lookup for Video details for each video report
+        {
+            $lookup: {
+                from: 'videos',
+                localField: 'targetId',
+                foreignField: '_id',
+                as: 'targetVideo',
+                pipeline: [
+                    {
+                        $project: {
+                            title: 1,
+                            thumbnail: 1
+                        }
+                    }
+                ]
+            }
+        },
+
+        // 2. Lookup for Post details for each post report
+        {
+            $lookup: {
+                from: 'posts',
+                localField: 'targetId',
+                foreignField: '_id',
+                as: 'targetPost',
+                pipeline: [
+                    {
+                        $project: { content: 1 }
+                    }
+                ]
+            }
+        },
+
+        // Now we choose which one to keep so that 'target' contains either video or post
+        {
+            $addFields: {
+                targetData: {
+                    $cond: {
+                        if: {
+                            $eq: ['$targetModel', 'Video']
+                        },
+                        then: { $first: '$targetVideo' },
+                        else: { $first: '$targetPost' }
+                    }
+                }
+            }            
+        },
+
+        // Remove unnecesary keys
+        {
+            $project: {
+                targetVideo: 0,
+                targetPost: 0
+            }
+        },
+
+        // Sort by latest reports
+        {
+            $sort: { createdAt: -1 }
+        }
+    ]);
+
+    const options = {
+        page: pageNo,
+        limit: limitCount
+    };
+
+    const result = await Report.aggregatePaginate(reportAggregate, options);
+
+    if(!result || result.docs.length === 0) {
+        return res.status(200).json(
+            new ApiResponse(200, result, 'No reports found.')
+        );
+    }
+
+    return res.status(200).json(
+        new ApiResponse(200, result, 'All reports are fetched successfully.')
+    );
+});
+
+
+
+export {
+    submitReport,
+    updateReportStatus,
+    getAllReports
+};
---
+++ src/models/report.model.ts
@@ -0,0 +1,62 @@
+import mongoose from 'mongoose';
+import type { Document, AggregatePaginateModel } from 'mongoose';
+import mongooseAggregatePaginate from 'mongoose-aggregate-paginate-v2';
+
+interface IReport {
+	reporter: mongoose.Types.ObjectId,
+	targetId: mongoose.Types.ObjectId,
+	targetModel: 'Video' | 'Post',
+	reason: 'Hate Speech' | 'Harassment' | 'Violence' | 'Spam' | 'Scam' | 'Inappropriate' | 'Other',
+	details?: string,
+	status: 'PEND' | 'REV' | 'RES' // 'PEND' -> 'Pending', 'REV' -> 'Reviewed', 'RES' -> 'Resolved'
+}
+
+type ReportDocument = IReport & Document;
+type ReportModel = AggregatePaginateModel<ReportDocument>;
+
+const reportSchema = new mongoose.Schema<ReportDocument, ReportModel>({
+	reporter: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'User',
+		required: [true, 'Reporter is required.']
+	},
+	// targetId can point to either a Video or a Post
+	targetId: {
+		type: mongoose.Schema.Types.ObjectId,
+		required: [true, 'ID (Video/Post) is required.'],
+		refPath: 'targetModel' // polymorphic relationship with 'refPath' to Video or Post model
+	},
+	// Limits reports to only Video or Post model
+	targetModel: {
+		type: String,
+		required: [true, 'Model (Video/Post) is required.'],
+		enum: ['Video', 'Post']
+	},
+	reason: {
+		type: String,
+		required: [true, 'Reason is required.'],
+		enum: ['Hate Speech', 'Harassment', 'Violence', 'Spam', 'Scam', 'Inappropriate', 'Other']
+	},
+	details: {
+		type: String,
+		trim: true
+	},
+	status: {
+		type: String,
+		required: [true, 'Status is required.'],
+		enum: ['PEND', 'REV', 'RES'],
+		description: "'PEND' -> 'Pending', 'REV' -> 'Reviewed', 'RES' -> 'Resolved'"
+	}
+}, { timestamps: true });
+
+// Adding a compound index to ensure that a user cannot report the same video/post twice (unique: true)
+// And also making the query searching faster with this index
+reportSchema.index({
+	reporter: 1,
+	targetId: 1
+}, { unique: true });
+
+reportSchema.plugin(mongooseAggregatePaginate as any);
+
+export const Report = mongoose.model<ReportDocument, ReportModel>('Report', reportSchema);
+export type { ReportDocument, ReportModel };
