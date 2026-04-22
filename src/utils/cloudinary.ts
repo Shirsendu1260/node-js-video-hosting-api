c597ad36b73c93ad52d117f83124aae689711fb6
Shirsendu Mali <shirsendu1260@gmail.com>
2026-04-22 10:33:10 +0530

    Modified cloudinary uploader for unlink error fix

--- src/utils/cloudinary.ts
+++ src/utils/cloudinary.ts
@@ -23,53 +23,48 @@ const cloudinaryUploader = async (
   localFilePath: string,
   subFolder: string
 ): Promise<UploadApiResponse | null> => {
-	try {
-		if(!localFilePath) return null;
+  if(!localFilePath) return null;
 
-    // Identify File Type
-    // We check the extension to decide if we should trigger 'Adaptive Bitrate Streaming' (HLS)
-    const isVideo = localFilePath.match(/\.(mp4|mov|avi|mkv|webm)$/i);
+  // Identify file type for a video
+  const isVideo = localFilePath.match(/\.(mp4|mov|avi|mkv|webm)$/i);
 
-    const options: UploadApiOptions = {
-      use_filename: true, // Use original filename as public_id (unique identifier of the uploaded file)
-      unique_filename: false, // Don't append random suffix to filename
-      overwrite: true, // Overwrite if same filename already exists
-      resource_type: "auto", // Auto-detect file type (image, video, raw)
-      chunk_size: 5000000, // 5MB per chunks (Ideal for slow connections)
-      timeout: 60000, // 60s timeout
-      folder: 'node-video-hosting-backend-uploads/' + subFolder
+  const options: UploadApiOptions = {
+    use_filename: true, // Use original filename as public_id (unique identifier of the uploaded file)
+    unique_filename: false, // Don't append random suffix to filename
+    overwrite: true, // Overwrite if same filename already exists
+    resource_type: 'auto', // Auto-detect file type (image, video, raw)
+    folder: 'node-video-hosting-backend-uploads/' + subFolder
+  };
 
-      // On a slow network, here is how our code handles a 200MB upload:
-      // Fragmentation: The file is sliced into 40 chunks (5MB each).
-      // Sequential Resilience: If the connection flickers during chunk no. 20, only that 5MB piece needs to 
-      //                        retry, not the entire 200MB.
-      // Timeouts: If the network is so slow that a single 5MB chunk takes longer than 60 seconds, 
-      //           the upload will fail and trigger our catch block.
+  // If it's a video, we tell Cloudinary to generate an .m3u8 manifest (for ABR/HLS)
+  // Cloudinary has many profiles (HD, Full_HD, etc.). Using 'low' limits the output to 320p (240p, 320p videos)
+  if(isVideo) {
+    options.transformation = {
+      streaming_profile: 'low',
+      format: 'm3u8'
     };
+    options.chunk_size = 5000000; // 5MB per chunks (ideal for slow connections)
+    options.timeout = 120000; // 2min timeout
 
-    // The 'Streaming Profile' Logic:
-    // If it's a video, we tell Cloudinary to generate an .m3u8 manifest (for ABR/HLS)
-    // Cloudinary has many profiles (HD, Full_HD, etc.). Using 'low' limits the output to 320p (240p, 320p videos)
-    if(isVideo) {
-      options.transformation = {
-        streaming_profile: "low",
-        format: "m3u8"
-      };
-    }
+    // upload_large: On a slow network, here is how our code handles a 200MB upload: The file is 
+    // sliced into 40 chunks (5MB each). If the connection flickers during chunk no. 20, only that 
+    // 5MB piece needs to retry, not the entire 200MB. If the network is so slow that a single 5MB 
+    // chunk takes longer than 60 seconds, the upload will fail and trigger our catch block.
 
     // Adaptive Bitrate Streaming (HLS):
-    // Instead of just serving a raw .mp4 link, we are generating a .m3u8 manifest.
-    // Instant Playback: Using the low streaming profile (targeting 240p/320p) ensures the video starts 
-    //                   playing almost instantly.
-    // Dynamic Scaling: The HLS player (like Hls.js) can automatically upgrade to higher quality if the 
-    //                  user's internet speed improves mid-watch.
+    // Instead of just serving a raw .mp4 link, we are generating a .m3u8 manifest. Using the low 
+    // streaming profile (targeting 240p/320p) ensures the video starts playing almost instantly.
+    // The HLS player (like Hls.js) can automatically upgrade to higher quality if the user's 
+    // internet speed improves during middle of watching.
+  }
 
+  let uploadResult: UploadApiResponse | null = null;
+
+	try {
     // Upload the local file from server to Cloudinary cloud storage
-    // 'upload_large' automatically splits files into chunks.
-    // If a 5MB chunk fails, it only retries that chunk, not the whole file.
-    const uploadResult = await cloudinary.uploader.upload_large(localFilePath, options) as UploadApiResponse;
-
-    fs.unlinkSync(localFilePath); // Remove (unlink) the saved temporary file from our local server storage
+    uploadResult = isVideo
+                   ? await cloudinary.uploader.upload_large(localFilePath, options) as UploadApiResponse
+                   : await cloudinary.uploader.upload(localFilePath, options) as UploadApiResponse;
     
     // Generate the Playback URL
     // secure_url (Static MP4): One single big file. Easy to use, but if the internet is slow, 
@@ -90,17 +85,16 @@ const cloudinaryUploader = async (
                                               .replace(/\.[^/.]+$/, '.m3u8');
     }
 
-    // Flow: 
-    // Client Uploads: User sends a video (e.g., 20MB MP4).
-    // Server Receives: Our Node.js server temporarily holds the file.
-    // Cloudinary 'upload_large': Our server sends the file in 5MB chunks.
-    // Transformation: Cloudinary receives the full file and immediately starts a 
-    //       background worker to slice it into tiny .ts segments (the HLS chunks).
-    // Deterministic Return: Our code doesn't wait for those chunks to be ready. 
-    //       It creates the 'Map' (playback_url) based on known Cloudinary patterns and returns 
-    //       it.
-    // Frontend Playback: When the user hits play, the HLS player reads the playback_url and starts 
-    //       fetching those tiny segments one by one.
+    // Flow when using 'upload_large': 
+    // 1. User sends a video (e.g., 20MB MP4).
+    // 2. Our Node.js server temporarily holds the file.
+    // 3. Our server sends the file in 5MB chunks.
+    // 4. Cloudinary receives the full file and immediately starts a background worker to slice 
+    //    it into tiny .ts segments (the HLS chunks).
+    // 5. Our code doesn't wait for those chunks to be ready. It creates the 'Map' (playback_url) 
+    //    based on known Cloudinary patterns and returns it.
+    // 6. Frontend Playback: When the user hits play, the HLS player reads the playback_url and 
+    //    starts fetching those tiny segments one by one.
 
     console.log('UPLOAD SUCCESSFUL ON CLOUDINARY. SOURCE:', uploadResult.secure_url);
 
@@ -130,14 +124,22 @@ const cloudinaryUploader = async (
     else {
       console.log('CLOUDINARY UPLOAD ERROR:', error);  
     }
-    
-    // Check if file exists before trying to delete it
-    if (fs.existsSync(localFilePath)) {
-        fs.unlinkSync(localFilePath)
-    }
 
     return null;
 	}
+  finally {
+    // 'finally' always runs whether upload succeeded or failed
+    // When 'finally' runs, upload_large stream is fully done
+    // So we can safely delete the file now
+    try {
+      if (fs.existsSync(localFilePath)) {
+          fs.unlinkSync(localFilePath)
+      }
+    }
+    catch(unlinkError) {
+      console.log('TEMP FILE DELETE ERROR:', unlinkError);  
+    }
+  }
 };
 
 // Delete uploaded file
