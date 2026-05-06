0ca9f68fecba3c092748447fccc88a06df43fa52
Shirsendu Mali <shirsendu1260@gmail.com>
2026-05-06 23:04:54 +0530

    Added JSDoc comment for healthcheck route

--- src/routes/healthcheck.routes.ts
+++ src/routes/healthcheck.routes.ts
@@ -3,6 +3,19 @@ import { healthcheck } from '../controllers/healthcheck.controller.js';
 
 const router = Router();
 
+/**
+ * @openapi
+ * /healthcheck:
+ *   get:
+ *     tags:
+ *       - Healthcheck
+ *     summary: Get standard 200 response for healthcheck.
+ *     responses:
+ *       200:
+ *         description: STATUS: ALL OK
+ *       429:
+ *         description: Rate limit exceeded.
+ */
 router.route('/').get(healthcheck);
 
 export default router;
